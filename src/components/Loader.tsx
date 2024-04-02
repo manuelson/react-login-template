5505113275a2960f3a66efc5af98b1213f9de192
Manu <manu.gonzalez@vicio.com>
2024-04-02 18:24:47 +0200

    add spinner loader

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,6 +1,11 @@
+import { Spinner } from "react-bootstrap";
 
 const Loader = () => {
-  return <div>Loading...</div>;
+  return (
+    <Spinner animation="border" role="status">
+      <span className="visually-hidden">Loading...</span>
+    </Spinner>
+  )
 };
 
 export default Loader;
