4d8a781cd2a7917f8af9f638a3ede013827cf891
Manu <manu.gonzalez@vicio.com>
2024-04-03 14:16:44 +0200

    Layout changes

--- src/layout/Index.tsx
+++ src/layout/Index.tsx
@@ -1,3 +1,4 @@
+import { Container } from "react-bootstrap";
 import { Outlet } from "react-router-dom";
 import Footer from "src/components/Footer";
 import NavBar from "src/components/NavBar";
@@ -7,12 +8,10 @@ export function Layout() {
   return (
     <>
         <NavBar />
-        <main className="mt-5 container">
+        <Container className="mt-5">
             <Outlet />
-        </main>
-
+        </Container>
         <Footer />
-
     </>
   )
 }
