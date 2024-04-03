76dfd27ea99a2e9913a5299a2cc250c8b45c3593
Manu <manu.gonzalez@vicio.com>
2024-04-03 16:57:46 +0200

    Add types in localstorage

--- src/hooks/useLocalStorage.tsx
+++ src/hooks/useLocalStorage.tsx
@@ -3,18 +3,18 @@ import { useState } from "react";
 export const useLocalStorage = () => {
   const [value, setValue] = useState<string | null>(null);
 
-  const setItem = (key: string, value: string) => {
+  const setItem = (key: string, value: string) : void => {
     localStorage.setItem(key, value);
     setValue(value);
   };
 
-  const getItem = (key: string) => {
+  const getItem = (key: string) : any => {
     const value = localStorage.getItem(key);
     setValue(value);
     return value;
   };
 
-  const removeItem = (key: string) => {
+  const removeItem = (key: string) : void => {
     localStorage.removeItem(key);
     setValue(null);
   };
