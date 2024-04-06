7c51761caa2f4825960b47382320eb51aa6af758
Manu <manu.gonzalez@vicio.com>
2024-04-06 19:06:46 +0200

    Add analytics for vercel

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,11 @@ import App from './App.tsx'
 import './index.css'
 import AuthProvider from "src/context/AuthContext";
 import 'font-awesome/css/font-awesome.min.css';
+import { Analytics } from "@vercel/analytics/react"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <AuthProvider.Provider>
         <App />
+        <Analytics />
     </AuthProvider.Provider>,
 )
