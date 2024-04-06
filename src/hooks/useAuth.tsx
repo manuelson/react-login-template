811c061e360030c4d3bf523f4ed5dcf1eb1ee46c
Manu <manu.gonzalez@vicio.com>
2024-04-06 17:38:01 +0200

    format and error control

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -1,44 +1,48 @@
-import { useEffect } from "react"
-import { useUser } from "src/hooks/useUser"
-import { useLocalStorage } from "src/hooks/useLocalStorage"
-import axios from 'axios'
-import { Response, LoginRequest } from "src/compiler/types"
-import { transformers } from "src/helpers/transformers"
+import { useEffect } from "react";
+import { useUser } from "src/hooks/useUser";
+import { useLocalStorage } from "src/hooks/useLocalStorage";
+import axios from "axios";
+import { Response, LoginRequest } from "src/compiler/types";
+import { transformers } from "src/helpers/transformers";
 
 export const useAuth = () => {
-  const { user, addUser, removeUser, setUser } = useUser()
-  const { getItem } = useLocalStorage()
-  const { transformUserResponse } = transformers()
+  const { user, addUser, removeUser, setUser } = useUser();
+  const { getItem } = useLocalStorage();
+  const { transformUserResponse } = transformers();
 
   useEffect(() => {
-    const storedUser = getItem("user")
+    const storedUser = getItem("user");
     if (storedUser) {
-      setUser(JSON.parse(storedUser))
+      setUser(JSON.parse(storedUser));
     }
-  }, [])
+  }, []);
 
-  const login = async (user: LoginRequest, setError: Function) : Promise<void> => {
-    await axios.post<Response>(`${import.meta.env.VITE_BACKEND_ENDPOINT}/auth/login`, {
-      email: user.email,
-      password: user.password
-    })
-    .then(response => {
-        addUser(transformUserResponse(response.data))
-    })
-    .catch(error => {
-      // Handle the error response
-      console.error(error)
-      if (!error.response && !error.response.data.message) {
-        setError(error.message)
-        return
-      }
-      setError(error.response.data.message)
-    })
-  }
+  const login = async (
+    user: LoginRequest,
+    setError: Function
+  ): Promise<void> => {
+    await axios
+      .post<Response>(`${import.meta.env.VITE_BACKEND_ENDPOINT}/auth/login`, {
+        email: user.email,
+        password: user.password,
+      })
+      .then((response) => {
+        addUser(transformUserResponse(response.data));
+      })
+      .catch((error) => {
+        // Handle the error response
+        console.error(error);
+        if (error.response && error.response.data.message) {
+          setError(error.response.data.message);
+        } else {
+          setError(error.message);
+        }
+      });
+  };
 
-  const logout = () : void => {
+  const logout = (): void => {
     removeUser();
-  }
+  };
 
-  return { user, login, logout, setUser }
+  return { user, login, logout, setUser };
 };
